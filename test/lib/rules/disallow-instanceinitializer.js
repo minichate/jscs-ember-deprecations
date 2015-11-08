268b780f53779af9adfc95c8371f760b34973406
Christopher Troup <ctroup@freshbooks.com>
2015-11-07 20:50:18 -0500

    Add tests for instances in initializers

---
+++ test/lib/rules/disallow-instanceinitializer.js
@@ -0,0 +1,69 @@
+describe('lib/rules/disallow-instanceinitializer', function () {
+    var checker = global.checker({
+        plugins: ['./lib/index']
+    });
+
+    describe('not configured', function() {
+
+      it('should report with undefined', function() {
+        global.expect(function() {
+          checker.configure({disallowInstanceInInitializer: undefined});
+        }).to.throws(/requires a true value/i);
+      });
+
+      it('should report with an object', function() {
+        global.expect(function() {
+          checker.configure({disallowInstanceInInitializer: {}});
+        }).to.throws(/requires a true value/i);
+      });
+
+    });
+
+    describe('with true', function() {
+      checker.rules({disallowInstanceInInitializer: true});
+
+      checker.cases([
+        /* jshint ignore:start */
+        {
+          it: 'should not report',
+          code: function() {
+            App.initializer({
+              name: "clock",
+
+              initialize: function(registry, application) {
+                application.register("clock:main", Clock);
+              }
+            });
+
+            App.instanceInitializer({
+              name: "clock",
+
+              initialize: function(instance) {
+                var clock = instance.container.lookup("clock:main");
+                clock.setStartTime(Date.now());
+              }
+            });
+          }
+        }, {
+          it: 'should report deprecated use',
+          errors: 1,
+          code: function() {
+            App.initializer({
+              name: "clock",
+
+              initialize: function(container, application) {
+                application.register("clock:main", Clock);
+                var clock = container.lookup("clock:main");
+                clock.setStartTime(Date.now());
+              }
+            });
+          },
+          errors: [{
+            column: 16, line: 6, filename: 'input', rule: 'disallowInstanceInInitializer', fixed: undefined,
+            message: 'Instances in initializers are deprecated in Ember 1.11'
+          }]
+        }
+        /* jshint ignore:end */
+      ]);
+    });
+});
