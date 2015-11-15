62eeb7f774f49d08d355abbd9e4418e0d8b695df
Christopher Troup <ctroup@freshbooks.com>
2015-11-14 21:05:24 -0500

    Refactor out some common code into helpers

---
+++ test/lib/helpers/deprecated-class.js
@@ -0,0 +1,17 @@
+var DeprecatedClass = require('../../../lib/helpers/deprecated-class');
+
+describe('lib/helpers/deprecated-class', function() {
+  it('should raise exception for getOptionName', function() {
+    global.expect(function() {
+      var sut = new DeprecatedClass();
+      sut.getOptionName();
+    }).to.throw('Needs to be implemented');
+  });
+
+  it('should raise exception for getErrorString', function() {
+    global.expect(function() {
+      var sut = new DeprecatedClass();
+      sut.getErrorString({});
+    }).to.throw('Needs to be implemented');
+  });
+});
