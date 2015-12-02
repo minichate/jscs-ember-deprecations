2b551210b35895dd695438d05d10705f717e37ef
Christopher Troup <ctroup@freshbooks.com>
2015-12-01 21:55:38 -0500

    Add support for Ember.computed.any() deprecation

--- README.md
+++ README.md
@@ -89,6 +89,8 @@ itself. See http://emberjs.com/deprecations/v1.x/#toc_set-code-positionalparams-
 
 - `disallowCreateWithMixins` will warn you if you use the deprecated `Ember.Object.createWithMixins` method. See http://emberjs.com/deprecations/v1.x/#toc_ember-createwithmixins for details.
 
+- `disallowComputedAny` will warn you if you use the deprecated `Ember.computed.any()` helper. See http://emberjs.com/deprecations/v1.x/#toc_ember-computed-any for details.
+
 ### Other Ember best practices
 
 - `disallowPrototypeExtension` will warn you if you are using `.property()`, `.observes()` or `observesBefore()`. See http://guides.emberjs.com/v1.10.0/configuring-ember/disabling-prototype-extensions/#toc_functions for details.
---
+++ test/lib/rules/disallow-computedany.js
@@ -0,0 +1,46 @@
+describe('lib/rules/disallow-computedany', function () {
+    var checker = global.checker({
+        plugins: ['./lib/index']
+    });
+
+    describe('not configured', function() {
+
+      it('should report with undefined', function() {
+        global.expect(function() {
+          checker.configure({disallowComputedAny: undefined});
+        }).to.throws(/requires a true value/i);
+      });
+
+      it('should report with an object', function() {
+        global.expect(function() {
+          checker.configure({disallowComputedAny: {}});
+        }).to.throws(/requires a true value/i);
+      });
+
+    });
+
+    describe('with true', function() {
+      checker.rules({disallowComputedAny: true});
+
+      checker.cases([
+        /* jshint ignore:start */
+        {
+          it: 'should not report Ember.computed.or()',
+          code: function() {
+            var Hamster = Ember.Object.extend({
+              hasClothes: Ember.computed.or('hat', 'shirt')
+            });
+          }
+        }, {
+          it: 'should report Ember.computed.any()',
+          errors: 1,
+          code: function() {
+            var Hamster = Ember.Object.extend({
+              hasClothes: Ember.computed.any('hat', 'shirt')
+            });
+          }
+        }
+        /* jshint ignore:end */
+      ]);
+    });
+});
