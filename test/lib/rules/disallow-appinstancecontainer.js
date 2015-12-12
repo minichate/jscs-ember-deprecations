36fc1b2b3d6c665e219a1ea0a4a5834a94fdb7d5
Christopher Troup <ctroup@freshbooks.com>
2015-12-12 17:43:19 -0500

    Add additional test cases to cover anonymous initializers

--- test/lib/rules/disallow-appinstancecontainer.js
+++ test/lib/rules/disallow-appinstancecontainer.js
@@ -40,6 +40,44 @@ describe('lib/rules/disallow-appinstancecontainer', function () {
             }
           }
         }, {
+          it: 'should not report appinstance.container in non-initializer path',
+          filename: 'app/bar/foo.js',
+          code: function() {
+            var initialize = function(appInstance) {
+              var store = appInstance.container.lookup('service:store');
+
+              store.pushPayload('<payload here>');
+            }
+
+            return {
+              name: 'preload-store',
+              initialize: initialize
+            }
+          }
+        }, {
+          it: 'should not report empty params',
+          filename: 'app/initializer/foo.js',
+          code: function() {
+            var initialize = function() {
+            }
+
+            return {
+              name: 'preload-store',
+              initialize: initialize
+            }
+          }
+        }, {
+          it: 'should not report anonymous initializer',
+          filename: 'app/initializer/foo.js',
+          code: function() {
+            return {
+              name: 'inject-session',
+              initialize: function(application) {
+                application.lookup('service:session');
+              }
+            }
+          }
+        }, {
           it: 'should report appinstance.container',
           filename: 'app/initializer/foo.js',
           errors: 1,
@@ -53,6 +91,18 @@ describe('lib/rules/disallow-appinstancecontainer', function () {
             return {
               name: 'preload-store',
               initialize: initialize
+            }
+          }
+        }, {
+          it: 'should report anonymous initializer',
+          filename: 'app/initializer/foo.js',
+          errors: 1,
+          code: function() {
+            return {
+              name: 'inject-session',
+              initialize: function(application) {
+                application.container.lookup('service:session');
+              }
             }
           }
         }
